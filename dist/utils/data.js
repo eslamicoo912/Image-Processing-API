d364eadfadfd5271311c97d052b21bb84c14ccf3
eslamicoo912 <eslamicoo3@gmail.com>
2022-09-19 10:44:29 +0200

    new images

--- dist/utils/data.js
+++ dist/utils/data.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.data = void 0;
-exports.data = ["berlin", "moscow", "newyork", "san"];
+exports.data = ['berlin', 'moscow', 'newyork', 'san'];
